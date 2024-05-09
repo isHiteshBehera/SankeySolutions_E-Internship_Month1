eaaad16b2652eb0720902445ea205146208b07d2
Hitesh Behera <bhiteshme74@gmail.com>
2024-05-10 00:18:16 +0530

    dotenv: for secure credentials

--- Month3/HotelAPI/GraphQL/Schema/postgre.js
+++ Month3/HotelAPI/GraphQL/Schema/postgre.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const dbConnection = new Sequelize('hotel_booking', 'db_user', 'db_password', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
+const dbConnection = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASS, 
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+);
 
 module.exports = dbConnection;
